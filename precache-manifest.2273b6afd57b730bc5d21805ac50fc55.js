375c45cdd24d9a37ca8a741e7f741544397a3dfe
NewItLevel <https://github.com/NewItLevel/ItChat.git>
2020-04-22 11:40:09 +0300

    Updates

---
+++ precache-manifest.2273b6afd57b730bc5d21805ac50fc55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2426390a17d90f2dfeff9b164423f34",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "85b53bd5cfa6bc0225ae",
+    "url": "/myapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b03128e17c4e2a806777",
+    "url": "/myapp/static/js/2.946a97f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.946a97f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b53bd5cfa6bc0225ae",
+    "url": "/myapp/static/js/main.32bb76e3.chunk.js"
+  },
+  {
+    "revision": "7c157e24c57b3c3aa05a",
+    "url": "/myapp/static/js/runtime-main.6d446693.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
